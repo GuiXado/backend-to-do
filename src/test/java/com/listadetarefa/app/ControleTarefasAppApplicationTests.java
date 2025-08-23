cb6cde4e17a12320fff9bff773b4c5d1e64752bd
Guilherme Machado <gui19880@hotmail.com>
2025-08-23 20:24:58 -0300

    init

---
+++ src/test/java/com/listadetarefa/app/ControleTarefasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listadetarefa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleTarefasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
